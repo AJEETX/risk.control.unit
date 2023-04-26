cca4875e223eea95e0fa39e878b28f6d7d688f66
ajeet <ajeetx@email.com>
2023-04-26 22:04:01 +1000

    client company

---
+++ WebApplication1/Models/ClientCompany.cs
@@ -0,0 +1,18 @@
+﻿namespace WebApplication1.Models
+{
+    public class ClientCompany
+    {
+        public string ClientCompanyId { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Code { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Branch { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public State? State { get; set; } 
+        public Country? Country { get; set; } 
+        public PinCode? PinCode { get; set; }
+    }
+}
