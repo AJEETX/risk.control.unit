1ab8776c03bf6ca37f2a936383dec9613f383080
ajeet <ajeetx@email.com>
2023-04-27 22:23:08 +1000

    pincode added

--- WebApplication1/Models/ClientCompany.cs
+++ WebApplication1/Models/ClientCompany.cs
@@ -8,11 +8,15 @@
         public string Description { get; set; } = string.Empty;
         public string PhoneNumber { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
+        public Address Address { get; set; } 
+
+    }
+    public class Address
+    {
         public string Branch { get; set; } = string.Empty;
-        public string Address { get; set; } = string.Empty;
         public string City { get; set; } = string.Empty;
-        public State? State { get; set; } 
-        public Country? Country { get; set; } 
+        public State? State { get; set; }
+        public Country? Country { get; set; }
         public PinCode? PinCode { get; set; }
     }
 }
